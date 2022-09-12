5d9d382d71399fcc368226f74c5c444110857647
Muhammad Abdulwanis <muhammad.abdulwanis@adesa.com>
2022-09-12 15:39:35 -0400

    We are finally able to read from the queue and write to the topic successfully.

---
+++ QueueTopicConnector/ReadQWriteToTopic.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using calculatorApi;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace QueueTopicConnector
+{
+    public class ReadQWriteToTopic
+    {
+        [FunctionName("ReadQWriteToTopic")]
+        public async Task RunAsync([ServiceBusTrigger("operation", Connection = "connection-string")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            var topicSender = new TopicSender();
+            await topicSender.Enqueue(myQueueItem);
+        }
+    }
+}
