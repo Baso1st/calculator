f68089577709936459aa0ed26f0fde1326999c9f
Muhammad Abdulwanis <muhammad.abdulwanis@adesa.com>
2022-09-13 12:08:23 -0400

    Writing to cosmos is finally working!!!

--- Models/Operation.cs
+++ Models/Operation.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Models
 {
     public class Equation
     {
-        public int Id { get; set; }
+        public string id { get; set; } = Guid.NewGuid().ToString();
         public string EquationId { get; set; } = Guid.NewGuid().ToString();
         public double X { get; set; }
         public double Y { get; set; }
